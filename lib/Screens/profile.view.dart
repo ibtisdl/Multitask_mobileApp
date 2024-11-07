e150069caaab7e72b3d90af184c650a93df5ed0c
ibtissam <ibtissamsaadali17gmail.com>
2024-11-07 12:32:47 +0100

    Third commit

--- lib/Screens/profile.view.dart
+++ lib/Screens/profile.view.dart
@@ -6,10 +6,37 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Icon(
-        Icons.person,
-        color: Colors.pink,
-        size: 40,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage("images/avatar.jpg"),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text("Ibtissam SAADALI",
+              style: TextStyle(fontSize: 30, color: Colors.indigo[900])),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "ibtisdl@emsi.ma",
+            style: TextStyle(fontSize: 30, color: Colors.indigo[900]),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 20,
+                backgroundColor: Colors.indigo[900],
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () {},
+              child: Text("Modifier Profil"))
+        ],
       ),
     );
   }
